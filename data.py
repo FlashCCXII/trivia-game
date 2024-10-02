ce85af9f4ca741e8ddcb01d73dc1e632128ffdcb
DOAjayi <125410155+FlashCCXII@users.noreply.github.com>
2024-10-02 03:58:15 -0500

    Initial commit.

---
+++ data.py
@@ -0,0 +1,101 @@
+question_data = [{"type":"boolean","difficulty":"easy","category":"Science &amp; Nature","question":"An average human can go two weeks without water.","correct_answer":"False","incorrect_answers":["True"]},
+    
+    {"type":"boolean","difficulty":"easy","category":"Entertainment: Video Games","question":"Ana was added as a new hero for the game Overwatch on July 19th, 2016.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"medium","category":"Entertainment: Video Games","question":"TF2: The Medic will be credited for an assist if he heals a Spy that successfully saps a building.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"medium","category":"Entertainment: Film","question":"The movie &quot;Tron&quot; received an Oscar nomination for Best Visual Effects.","correct_answer":"False","incorrect_answers":["True"]},
+    
+    {"type":"boolean","difficulty":"medium","category":"Sports","question":"ATP tennis hosted several tournaments on carpet court before being replaced to reduce injuries.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"medium","category":"Geography","question":"Vietnam is the only country in the world that starts with V. ","correct_answer":"False","incorrect_answers":["True"]},
+    
+    {"type":"boolean","difficulty":"medium","category":"Animals","question":"The average lifespan of a wildcat is only around 5-6 years. ","correct_answer":"False","incorrect_answers":["True"]},
+    
+    {"type":"boolean","difficulty":"easy","category":"History","question":"The French Kingdom helped the United States gain their independence over Great Britain during the Revolutionary War.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"easy","category":"Science &amp; Nature","question":"Igneous rocks are formed by excessive heat and pressure.","correct_answer":"False","incorrect_answers":["True"]},
+    
+    {"type":"boolean","difficulty":"medium","category":"Politics","question":"George W. Bush lost the popular vote in the 2004 United States presidential election.","correct_answer":"False","incorrect_answers":["True"]},
+    
+    {"type":"boolean","difficulty":"easy","category":"Entertainment: Video Games","question":"The game &quot;Pocket Morty&quot; has a Morty called &quot;Pocket Mortys Morty&quot;?","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"easy","category":"Entertainment: Video Games","question":"In Team Fortress 2, being disguised as a scout or medic results in a speed boost.","correct_answer":"False","incorrect_answers":["True"]},
+    
+    {"type":"boolean","difficulty":"medium","category":"Entertainment: Cartoon &amp; Animations","question":"In &quot;Avatar: The Last Airbender&quot; and &quot;The Legend of Korra&quot;, Lavabending is a specialized bending technique of Firebending.","correct_answer":"False","incorrect_answers":["True"]},
+    
+    {"type":"boolean","difficulty":"medium","category":"Entertainment: Video Games","question":"In career mode of &quot;Need for Speed: Underground 2&quot;, the first car the player can drive is the BMW M3 GTR.","correct_answer":"False","incorrect_answers":["True"]},
+    
+    {"type":"boolean","difficulty":"easy","category":"General Knowledge","question":"Romanian belongs to the Romance language family, shared with French, Spanish, Portuguese and Italian. ","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"medium","category":"Science: Computers","question":"All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.","correct_answer":"False","incorrect_answers":["True"]},
+    
+    {"type":"boolean","difficulty":"medium","category":"Entertainment: Film","question":"Sean Connery wasn&#039;t in &quot;Indiana Jones and the Kingdom of the Crystal Skull&quot; because he found retirement too enjoyable.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"easy","category":"Geography","question":"Greenland is almost as big as Africa.","correct_answer":"False","incorrect_answers":["True"]},
+    
+    {"type":"boolean","difficulty":"hard","category":"General Knowledge","question":"Spoon theory is a theory, utilizing &quot;Spoons&quot; as a metaphor for energy they can use in a day.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"medium","category":"Geography","question":"The capital of the US State Ohio is the city of Chillicothe.","correct_answer":"False","incorrect_answers":["True"]},
+    
+    {"type":"boolean","difficulty":"easy","category":"Entertainment: Video Games","question":"Deus Ex (2000) does not feature the World Trade Center because it was destroyed by terrorist attacks according to the game&#039;s plot.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"easy","category":"Science: Computers","question":"The NVidia GTX 1080 gets its name because it can only render at a 1920x1080 screen resolution.","correct_answer":"False","incorrect_answers":["True"]},
+    
+    {"type":"boolean","difficulty":"medium","category":"Entertainment: Film","question":"The original ending of &quot;Little Shop Of Horrors&quot; has the plants taking over the world.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"easy","category":"Science &amp; Nature","question":"An atom contains a nucleus.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"medium","category":"History","question":"In 1967, a magazine published a story about extracting hallucinogenic chemicals from bananas to raise moral questions about banning drugs.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"hard","category":"Entertainment: Board Games","question":"The board game Go has more possible legal positions than the number of atoms in the visible universe.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"easy","category":"Science: Mathematics","question":"An isosceles triangle has two sides of equal length as opposed to three.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"medium","category":"History","question":"The M41 Walker Bulldog remains in service in some countries to this day.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"easy","category":"Entertainment: Video Games","question":"&quot;Undertale&quot; is an RPG created by Toby Fox and released in 2015.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"hard","category":"Mythology","question":"Rannamaari was a sea demon that haunted the people of the Maldives and had to be appeased monthly with the sacrifice of a virgin girl.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"hard","category":"Politics","question":"Nazi Germany surrendered on Harry Truman&#039;s birthday while he was president.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"easy","category":"Entertainment: Video Games","question":"English new wave musician Gary Numan founded the video game development company Facepunch Studios in March 2009.","correct_answer":"False","incorrect_answers":["True"]},
+    
+    {"type":"boolean","difficulty":"medium","category":"Entertainment: Video Games","question":"David Baszucki was a co-founder of ROBLOX Corporation.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"easy","category":"Geography","question":"There is an island in Japan called \u014ckunoshima, A.K.A. &quot;Rabbit Island&quot;, so named because of it&#039;s huge population of rabbits.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"easy","category":"Entertainment: Video Games","question":"In Kingdom Hearts the Paopu fruit is said to intertwine the destinies for two people forever.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"medium","category":"General Knowledge","question":"The scientific name for the Southern Lights is Aurora Australis?","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"medium","category":"Entertainment: Video Games","question":"The Ace Attorney trilogy was suppose to end with &quot;Phoenix Wright: Ace Attorney &minus; Trials and Tribulations&quot; as its final game.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"medium","category":"General Knowledge","question":"Furby was released in 1998.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"medium","category":"Entertainment: Books","question":"Originally, the character Charlie from Charlie and the Chocolate Factory was going to be black.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"easy","category":"Entertainment: Video Games","question":"The name of the main character of the video game &quot;The Legend of Zelda&quot;, is Zelda.","correct_answer":"False","incorrect_answers":["True"]},
+    
+    {"type":"boolean","difficulty":"medium","category":"General Knowledge","question":"Coca-Cola&#039;s original colour was green.","correct_answer":"False","incorrect_answers":["True"]},
+    
+    {"type":"boolean","difficulty":"easy","category":"Entertainment: Video Games","question":"The Indie Game Development Studio Cing, developers of Hotel Dusk and Last Window, went bankrupt on March 1st, 2010.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"hard","category":"Entertainment: Video Games","question":"The video game &quot;Fuel&quot; has an open world that is 5,560 square miles?","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"easy","category":"Entertainment: Video Games","question":"&quot;Sonic the Hedgehog 2&quot; originally was going to have a time travel system.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"medium","category":"History","question":"Sargon II, a king of the Neo-Assyrian Empire, was a direct descendant of Sargon of Akkad.","correct_answer":"False","incorrect_answers":["True"]},
+    
+    {"type":"boolean","difficulty":"hard","category":"Science &amp; Nature","question":"The chemical element Lithium is named after the country of Lithuania.","correct_answer":"False","incorrect_answers":["True"]},
+    
+    {"type":"boolean","difficulty":"medium","category":"Entertainment: Music","question":"3 members of 2 Live Crew were arrested for playing songs from their album, As Nasty As They Wanna Be, live.","correct_answer":"True","incorrect_answers":["False"]},
+    
+    {"type":"boolean","difficulty":"medium","category":"Entertainment: Music","question":"&quot;Twenty One Pilots&quot; made the song &quot;The Motion&quot; featuring Sampha.","correct_answer":"False","incorrect_answers":["True"]},
+    
+    {"type":"boolean","difficulty":"easy","category":"Entertainment: Film","question":"Brandon Routh plays the titular character in the movie &quot;John Wick&quot;.","correct_answer":"False","incorrect_answers":["True"]},
+    
+    {"type":"boolean","difficulty":"easy","category":"Entertainment: Musicals &amp; Theatres","question":"IMAX stands for Image Maximum.","correct_answer":"True","incorrect_answers":["False"]}
+]
+    
